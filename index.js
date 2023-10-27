391f8548084d85b154cee7988f63a5b8a903739e
SumiAkter2 <akteralisha36@gmail.com>
2023-10-27 21:47:56 +0600

    added table

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ function getAmount(quantityValue, unitaryValue) {
   var unitary = parseFloat(unitaryId.value);
   let total = 0;
   total = quantity * unitary;
+  document.getElementById("cart-show").style.display = "block";
+  document.getElementById("cart-span").style.display = "none";
   document.getElementById("amount").value = total;
+  document.getElementById("subtotal").style.display = "flex";
+  document.getElementById("total-amount").innerText = `R$. ${total}`;
+  document.getElementById("unitary-total").innerText = total;
+  document.getElementById("value-total").innerText = total;
+
   console.log(quantity, unitary);
 }
